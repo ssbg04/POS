74b7eb85e2bbcb7df8de39dd1807253cabaf20c5
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-11-13 19:12:05 +0800

    Delete src directory

--- src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// const API_URL = 'http://localhost:3001/api/auth';
-const API_URL = 'http://192.168.0.104:3001/api/auth';
-
-export const authEmployee = async (username, password) => {
-    const res = await axios.post(API_URL, { username, password });
-    return res.data;
-};
